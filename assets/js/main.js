8026c2530715a3d260ca0056b954b3f32ff0a5d3
alexvcs <alexcurtisslep@gmail.com>
2022-11-03 18:05:35 -0400

    rounded project image corners, added setTimeout for email copied messaged, update screenshot in the readMe, added open graph content

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,45 +4,47 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-const successMessage = document.getElementById("success-msg")
+const successMessage = document.getElementById("success-msg");
 
 function copyEmail() {
-	const myEmail = document.getElementById("email").innerText;
+  const myEmail = document.getElementById("email").innerText;
 
-	navigator.clipboard.writeText(myEmail).then(() => {
-		console.log("Copied to clipboard");
-	});
-	
-	successMessage.style.display = "block";
+  navigator.clipboard.writeText(myEmail).then(() => {
+    console.log("Copied to clipboard");
+  });
 
+  successMessage.style.display = "block";
+
+  setTimeout(() => {
+    successMessage.style.display = "none";
+  }, 2500);
 }
 
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $nav = $("#nav");
 
-(function($) {
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: [null, "736px"],
+  });
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
-
+  // Scrolly.
+  $("#nav a, .scrolly").scrolly({
+    speed: 1000,
+    offset: function () {
+      return $nav.height();
+    },
+  });
 })(jQuery);
